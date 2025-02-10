bb475066effc11ecbcf63d50831eff73a8048f36
Volq <kirill@volq.ru>
2025-02-10 19:25:45 +0300

    Module 3. Complete

---
+++ Geometry_3_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Geometry
+{
+    enum Figures { Romb, Rect, RightTriangle, Circle }
+    struct Fdata
+    {
+        public int x0, y0;
+        public double a, b;
+        public int color;
+        public Figures type;
+    }
+    class Program
+    {
+        static double area(Fdata fd)
+        {
+            double res = 0;
+            res = fd.type switch
+            {
+                Figures.Romb => fd.a * fd.b, // a - сторона ромба, b - высота
+                Figures.Rect => fd.a * fd.b, // a, b - стороны прямоугольника
+                Figures.RightTriangle => (fd.a * fd.a * Math.Sqrt(3)) / 4, // a - сторона правильного треугольника
+                Figures.Circle => Math.PI * fd.a * fd.a // a - радиус окружности
+            };
+            return res;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Fdata fd = new Fdata() { a = 4, b = 3, type = Figures.Romb };
+            Console.WriteLine(area(fd));
+            Console.WriteLine("Ok");
+        }
+    }
+}
---
+++ module_3_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace module_3_1
+{
+
+    internal class Program
+    {
+        static long ffactorial(int n)
+        {
+            long res = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+
+        static long rfactorial(int n)
+        {
+            return n < 1 ? 1 : n * rfactorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ffactorial(10));
+            Console.WriteLine(rfactorial(10));
+        }
+    }
+}
