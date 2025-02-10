64d0321069fe5976eae316ec8b64465ef0ea870d
Volq <kirill@volq.ru>
2025-02-10 19:32:46 +0300

    Module 4. Completed

---
+++ module_4_1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace module_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args) // перечисление входных параметров в виде строк
+            {
+                Console.WriteLine(arg); 
+            }
+
+            int i;
+/*            foreach (string arg in args) // если во входном параметре будет алфовитный символ, то возвращается исключение
+            {
+                i = int.Parse(arg);
+                Console.WriteLine(i);
+            }*/
+
+            foreach (string arg in args) // Проверка  с условием
+            {
+                if (int.TryParse(arg, out i))
+                    Console.WriteLine(i);
+                else Console.WriteLine($"{arg} - Не число");
+            }
+
+        }
+    }
+}
---
+++ module_4_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace module_4_1dop
+{
+    internal class Program
+    {
+        static string IntToBinary(int num)
+        {
+            int bit;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 32; i++)
+            {
+                if (i % 8 == 0) sb.Insert(0, " ");
+                bit = (num >> i & 1) == 1 ? 1 : 0;
+                sb.Insert(0,bit);
+            }
+            return sb.ToString();
+        }
+        static void Main(string[] args)
+        {
+            int num = 35145;
+            Console.WriteLine($"{num} = {IntToBinary(num)}");
+        }
+    }
+}
