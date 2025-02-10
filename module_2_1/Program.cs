bfaa06b78a0c917dc15d04f1c23c26ea86c10e22
Volq <kirill@volq.ru>
2025-02-10 19:21:54 +0300

    Module 2. Complete

---
+++ module_2_1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace module_2_1
+{
+    enum Figures
+    {
+        Romb,
+        Circle,
+        Rect,
+        Square,
+        Line
+    }
+
+    struct Fdata
+    {
+        public int x0, y0;      // координаты центра тяжести
+        public int color;       // цвет фигуры
+        public Figures type;    // тип фигуры
+        public void PrintFdata()
+        {
+            Console.WriteLine($"Типа фигуры:\t{type}\n" +
+                $"Координаты центра тяжести:\tx = {x0}, y = {y0}\n" +
+                $"Цвет фигуры:\t{color}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fdata fd1 = new Fdata()
+            {
+                x0 = 1,
+                y0 = 1,
+                color = 0x2727ff,
+                type = Figures.Romb,
+            };
+
+            fd1.PrintFdata(); // Выводим в консоль данные по fd1
+        }
+    }
+}
