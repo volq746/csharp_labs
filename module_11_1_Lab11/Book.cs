5d6ee7ffdf49cc21e532138b87da6b05de2ac34c
Volq <kirill@volq.ru>
2025-02-16 16:57:27 +0300

    Module 11. Completed

---
+++ module_11_1_Lab11/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Lab11
+{
+    class Book : IComparable
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return "id=" + Id + " price=" + Price + "$  " + Author + " : " + Title;
+        }
+
+        // IComparable
+        public int CompareTo(Object? obj)
+        {
+            return (int)(this.Price - (obj as Book).Price);
+        }
+
+        class SortByAuthor : IComparer
+        {
+            public int Compare(Object x, Object y)
+            {
+                int res = 0;
+                Book b1 = (Book)x;
+                Book b2 = (Book)y;
+
+                res = b1.Author.CompareTo(b2.Author);
+                if (res == 0)
+                {
+                    res = (int)(b1.Price - b2.Price);
+                }
+                return res;
+            }
+        }
+
+        public static IComparer ByAuthor()
+        {
+            return new SortByAuthor();
+        }
+    }
+}
---
+++ module_11_1_Lab11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList books = new ArrayList();
+            //List<Book> books = new List<Book>(); // Типизируем коллекцию для сортировки IComparable
+
+            books.Add(new Book(){Id=1, Price=32, Author="Seemann", Title="Dependency Injection in .NET" });
+            books.Add(new Book() {Id=2, Price=42, Author="Richter", Title="CLR via C#" });
+            books.Add(new Book() {Id=3, Price=21, Author="Munro", Title="ASP.NET MVC 5" });
+            books.Add(new Book() {Id=4, Price =39, Author="Esposito", Title= "Microsoft® ASP.NET and AJAX" });
+            books.Add(new Book() {Id=5, Price=30, Author="Esposito", Title="Architecting Applications for the Enterprise" });
+            books.Add(new Book() {Id=6, Price=23, Author="Watson", Title="Writing High-Performance .NET Code" });
+            books.Add(new Book() {Id=7, Price=37, Author="Sharp", Title="Microsoft Visual C# 2013" });
+            books.Add(new Book() {Id=8, Price=20, Author="Esposito", Title= "Programming Microsoft® ASP.NET 2.0" });
+            books.Add(new Book() {Id=9, Price=11, Author="Stubblebine", Title="Regular Expression" });
+            books.Add(new Book() {Id=10, Price=27, Author="Liberty", Title="Learning Visual Basic .Net" });
+            books.Add(new Book() {Id=11, Price=62, Author="Cwalina", Title="Framework Design Guidelines" });
+            books.Add(new Book() {Id=12, Price=55, Author="Blewett", Title="Pro Asynchronous Programming with .NET" });
+            books.Add(new Book() {Id=13, Price=47, Author="Nathan", Title="Windows Presentation Foundation Unleashed" });
+
+            books.Sort(Book.ByAuthor());
+            foreach (Book book in books) Console.WriteLine(book);
+        }
+    }
+}
